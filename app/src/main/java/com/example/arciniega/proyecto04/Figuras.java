697218a86a9159c3e78ddf694e75f977343fc1a7
Chubuntu Arciniega <arciniega.chubuntu@gmail.com>
2016-06-24 17:56:13 -0600

    Creación de la clase Figuras

---
+++ app/src/main/java/com/example/arciniega/proyecto04/Figuras.java
@@ -0,0 +1,46 @@
+package com.example.arciniega.proyecto04;
+
+/**
+ * Created by arciniega on 24/06/16.
+ */
+public class Figuras {
+
+    //Declaración de variables
+    private int valor1;
+    private int valor2;
+    private int resultado;
+    private String figura;
+
+    //Constructor de la clase
+    public Figuras(){
+
+    }
+
+    public int getValor1() {
+        return valor1;
+    }
+
+    public void setValor1(int valor1) {
+        this.valor1 = valor1;
+    }
+
+    public int getValor2() {
+        return valor2;
+    }
+
+    public void setValor2(int valor2) {
+        this.valor2 = valor2;
+    }
+
+    public String getFigura() {
+        return figura;
+    }
+
+    public void setFigura(String figura) {
+        this.figura = figura;
+    }
+
+    public static void calculo(){
+
+    }
+}
